d89456dd2f2b988439d980866a1827952e03d222
Natalie Leung <40549274+polarcow285@users.noreply.github.com>
2020-07-13 13:10:13 -0700

    fix binary for next time

--- arduinoWifi.ino
+++ arduinoWifi.ino
@@ -1,66 +1,54 @@
+/*
+ * Sketch: ESP8266_LED_Control_02
+ * Control an LED from a web browser
+ * Intended to be run on an ESP8266
+ * 
+ * connect to the ESP8266 AP then
+ * use web broswer to go to 192.168.4.1
+ * 
+ */
+//update when you add a button
+#define NUMBER_OF_BUTTONS 14
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <Servo.h>
-Servo panMotor;
-Servo tiltMotor;
-Servo rollMotor;
-/* Set these to your desired credentials. */
-const char *ssid = "Leung"; //Enter your WIFI ssid
-const char *password = "nathannatalie"; //Enter your WIFI password
+const char WiFiPassword[] = "12345678";
+const char AP_NameChar[] = "LEDControl" ;
+const int sizeOfWebPage = NUMBER_OF_BUTTONS + 3;
+int dialMode = 0;
+int formIndex = 0;
 int panMotorPosition = 90;
 int tiltMotorPosition = 90;
 int rollMotorPosition = 90;
-bool dialMode = false;
-ESP8266WebServer server(80);
-//buttonName = "LED_BUILTIN_on"
-//formNumber = "formX"
-String buttonHelper(String buttonName, String formNumber){
+Servo panMotor;
+Servo tiltMotor;
+Servo tilt2Motor;
+Servo rollMotor;
+Servo clawMotor;
+
+
+WiFiServer server(80);
+String webPage[sizeOfWebPage];
+
+
+String request = "";
+int LED_Pin = 5;
+//buttonName = LEDON
+//formNumber = Fx
+
+String buttonHelper(String buttonName, int formNumber){
   String code = "";
-  code += "<form action=\"/" + buttonName + "\""; 
-  code += " method=\"get\"";
-  code += " id=\"" + formNumber + "\"></form>";
-  code += "<button type=\"submit\"";
-  code += "form=\"" + formNumber + "\"";
-  code += "value=\"" + buttonName +"\"";
-  code += ">" + buttonName + "</button>";
+  String formString = formHelper(formNumber);
+  code += "<form id='" + formString + "' action='" + buttonName + "'>";
+  code += "<input class='button' type='submit' value='" + buttonName + "' ></form><br>";
   return code;  
 }
 
-void handleRoot() {
- String buttons = "";
- buttons += buttonHelper("LED_BUILTIN_on", "form1");
- buttons += buttonHelper("LED_BUILTIN_off", "form2");
- buttons += buttonHelper("OtherLED_on", "form3");
- buttons += buttonHelper("Pan", "form4");
- buttons += buttonHelper("Pan90", "form5");
- buttons += buttonHelper("Pan+", "form6");
- buttons += buttonHelper("Pan-", "form7");
- buttons += buttonHelper("Tilt+", "form8");
- buttons += buttonHelper("Tilt-", "form9");
- buttons += buttonHelper("Roll+", "form10");
- buttons += buttonHelper("Roll-", "form11");
- buttons += buttonHelper("RGBGreen", "form12");
- buttons += buttonHelper("RGBRed", "form13");
- buttons += buttonHelper("RGBBlue", "form14");
- buttons += buttonHelper("RGBRaspberry", "form15");
- buttons += buttonHelper("DialMode", "form16");
- buttons += buttonHelper("SwitchDialMode", "form17");
- 
- 
- server.send(200, "text/html", buttons );
+String formHelper(int index){
+  String formNumber;
+  formNumber = "F" + String(index);
+  return formNumber;
 }
-void handleSave() {
- if (server.arg("pass") != "") {
-   Serial.println(server.arg("pass"));
- }
-}
-void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
- {
-  analogWrite(12, red_light_value);
-  analogWrite(4, green_light_value);
-  analogWrite(16, blue_light_value);
-}
+
 float translateA0(int analogValue){
   float voltage;
   voltage = (float)(5.0/1024.0) * (float)analogValue;
@@ -73,185 +61,146 @@ float translateVoltage(int analogValue){
   return(motorPosition);
 }
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(A0, INPUT); 
-  pinMode(0, OUTPUT); //panMotor
-  pinMode(2, OUTPUT); //rollMotor
-  pinMode(4, OUTPUT); //RGB Green
-  pinMode(5,OUTPUT); //LED
-  pinMode(14, OUTPUT); //tiltMotor
-  pinMode(12, OUTPUT); //RGB Red
-  pinMode(16, OUTPUT); //RGB Blue
-  panMotor.attach(0);
-  tiltMotor.attach(14);
-  rollMotor.attach(2);
+void setup() 
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(A0, INPUT); 
+    pinMode(5,OUTPUT); //LED
+    pinMode(0, OUTPUT); //panMotor
+    pinMode(2, OUTPUT); //tiltMotor
+    pinMode(14, OUTPUT); //tilt2Motor
+    pinMode(12, OUTPUT); //rollMotor
+    pinMode(13, OUTPUT); //clawMotor
+    pinMode(LED_Pin, OUTPUT); 
     
-  delay(3000);
-  Serial.begin(115200);
-  Serial.println();
-  Serial.print("Configuring access point...");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
- 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.on ( "/", handleRoot );
-  server.on ("/save", handleSave);
-  server.begin();
-  Serial.println ( "HTTP server started" );
-
-  panMotor.write(panMotorPosition);
-  server.on("/LED_BUILTIN_on", []() {
-    for(int i = 0; i<10; i++){
-      digitalWrite(LED_BUILTIN, 0);
-      Serial.println("on");
-      delay(1000);
-      digitalWrite(LED_BUILTIN, 1);
-      Serial.println("off");
-      delay(1000); 
-    }
-    Serial.println(LED_BUILTIN);  
-  });
-  server.on("/OtherLED_on", [](){
-   digitalWrite(4,1);
-   handleRoot();
-  });
- 
-  server.on("/LED_BUILTIN_off", []() {
-    //digitalWrite(LED_BUILTIN, 1);
-    Serial.println("off");
-    digitalWrite(4,0);
-    handleRoot();
-  });
- 
-  server.on("/Pan", [](){
-    if (dialMode == false){
-      panMotor.write(0);
-      handleRoot();
-    }
-  });
- 
-  server.on("/Pan90", [](){
-    if (dialMode == false){
-      panMotor.write(90);
-      handleRoot();
-    }
-  });
- 
-  server.on("/Pan+", [](){
-    if (dialMode == false){
-      panMotorPosition += 5;
-      panMotor.write(panMotorPosition);
-      handleRoot();
-    }
-  
-  });
- 
-  server.on("/Pan-", [](){
-    if (dialMode == false){
-      panMotorPosition -= 5;
-      panMotor.write(panMotorPosition);
-      handleRoot();
-    }
-  });
- 
-  server.on("/Tilt+", [](){
-    tiltMotorPosition += 5;
-    tiltMotor.write(tiltMotorPosition);
-    handleRoot();
-  });
- 
-  server.on("/Tilt-", [](){
-    tiltMotorPosition -= 5;
-    tiltMotor.write(tiltMotorPosition);
-    handleRoot();
-  });
- 
-  server.on("/Roll+", [](){
-    rollMotorPosition += 5;
-    rollMotor.write(rollMotorPosition);
-    handleRoot();
-  });
- 
-  server.on("/Roll-", [](){
-    rollMotorPosition -= 5;
-    rollMotor.write(rollMotorPosition);
-    handleRoot();
-  });
- 
-  server.on("/RGBGreen", [](){
-    digitalWrite(5, 1);
-    handleRoot();
-  });
- 
-  server.on("/RGBRed", [](){
-    RGB_color(255, 0, 0);
-    handleRoot();
-  });
- 
-  server.on("/RGBBlue", [](){
-    digitalWrite(16, 1);
-    handleRoot();
-  });
- 
-  server.on("/RGBRaspberry", [](){
-    RGB_color(255, 255, 125); // Raspberry
+    panMotor.attach(0);
+    tiltMotor.attach(2);
+    tilt2Motor.attach(14);
+    rollMotor.attach(12);
+    clawMotor.attach(13);
+    
+    int index = 0;
+    
+    webPage[index] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+    webPage[++index] = "<!DOCTYPE html><html><head><title>LED Control</title></head><body><div id='main'><h2>LED Control</h2>";
+    webPage[++index] = buttonHelper("LEDON", ++formIndex);
+    webPage[++index] = buttonHelper("LEDOFF", ++formIndex);
+    webPage[++index] = buttonHelper("LedBuiltInOn", ++formIndex);
+    webPage[++index] = buttonHelper("LedBuiltInOff", ++formIndex);
+    webPage[++index] = buttonHelper("Pan0", ++formIndex);
+    webPage[++index] = buttonHelper("Pan90", ++formIndex);
+    webPage[++index] = buttonHelper("Pan+", ++formIndex);
+    webPage[++index] = buttonHelper("Pan-", ++formIndex);
+    //webPage[++index] = buttonHelper("SwitchDialMode", ++formIndex);
+    webPage[++index] = buttonHelper("Tilt+", ++formIndex);
+    webPage[++index] = buttonHelper("Tilt-", ++formIndex);
+    webPage[++index] = buttonHelper("PanDialMode", ++formIndex);
+    webPage[++index] = buttonHelper("TiltDialMode", ++formIndex);
+    webPage[++index] = buttonHelper("DisableAll", ++formIndex);
+    webPage[++index] = buttonHelper("EnableAll", ++formIndex);
+    
+    webPage[sizeOfWebPage-1] = "</div></body></html>";
+    
+    boolean conn = WiFi.softAP(AP_NameChar, WiFiPassword);
+    Serial.begin(115200);
+    server.begin();
+    Serial.println("Initiating the loop");
     delay(1000);
-    handleRoot();
-  });
- 
-  server.on("/DialMode", [](){
-    /*while (dialMode == true){
-      Serial.println(translateVoltage(analogRead(A0)));
-      panMotor.write(translateVoltage(analogRead(A0)));
-      delay(100);
-      handleRoot();
-      if (dialMode == false){
-        break;
+} // void setup()
+
+
+void loop() 
+{
+
+    // Check if a client has connected
+    WiFiClient client = server.available();
+    if (!client)  {
+      //This needs to be here because this part loops as the site is waiting for a button press.
+      if (dialMode == 1 || dialMode == 3){
+        Serial.println(translateVoltage(analogRead(A0)));
+        tiltMotor.write(translateVoltage(analogRead(A0)));
+      } 
+      if (dialMode == 2 || dialMode == 3){
+        Serial.println(translateVoltage(analogRead(A0)));
+        tilt2Motor.write(translateVoltage(analogRead(A0)));
+      } 
+      //Serial.println("returning from loop");
+      return;  
+    }
+    
+
+    // Read the first line of the request
+    //This will wait here until a button is pressed
+    request = client.readStringUntil('\r');
+    
+    if       ( request.indexOf("LEDON") > 0 )  { digitalWrite(LED_Pin, HIGH);  }
+    else if  ( request.indexOf("LEDOFF") > 0 ) { digitalWrite(LED_Pin, LOW);   }
+    else if  ( request.indexOf("LedBuiltInOn") > 0 )  { digitalWrite(LED_BUILTIN, LOW);  }
+    else if  ( request.indexOf("LedBuiltInOff") > 0 )  { digitalWrite(LED_BUILTIN, HIGH);  }
+    
+    else if  ( request.indexOf("Pan0") > 0 )  { 
+      if (dialMode == 0){
+        tiltMotor.write(0);
       }
+       
+    }
+    else if  ( request.indexOf("Pan90") > 0 )  { 
+      if (dialMode == 0){
+        tiltMotor.write(180);
+      }
+        
+    }
+    else if  ( request.indexOf("Pan+") > 0 )  { 
+      if (dialMode == 0) {
+        panMotorPosition += 5;
+        panMotor.write(panMotorPosition);  
+      }
+      
+    }
+    else if  ( request.indexOf("Pan-") > 0 )  { 
+      if (dialMode == 0){
+        panMotorPosition -= 5;
+        panMotor.write(panMotorPosition);  
+      }
+      
+    }
+    /*else if  ( request.indexOf("SwitchDialMode") > 0 )  { 
+      dialMode = !dialMode;
+      (dialMode == true) ? Serial.println("Dial Mode: On") : Serial.println("Dial Mode: Off");
     }*/
-   });
-  server.on("/SwitchDialMode", [](){
-    dialMode = !dialMode;
-    (dialMode == true) ? Serial.println("Dial Mode: On") : Serial.println("Dial Mode: Off");
-    handleRoot();
-  });
- 
-}
-void loop() {
-  server.handleClient();
-  //Serial.println("Hello");
-  //delay(1000);
-  /*if (dialMode == true){
-    Serial.println(translateVoltage(analogRead(A0)));
-  }
-  */
+    else if  ( request.indexOf("Tilt+") > 0 )  { 
+       tiltMotorPosition += 5;
+       tiltMotor.write(tiltMotorPosition);      
+    }
+    else if  ( request.indexOf("Tilt-") > 0 )  { 
+        tiltMotorPosition -= 5;
+        tiltMotor.write(tiltMotorPosition);    
+    }
+    else if  ( request.indexOf("PanDialMode") > 0 )  { 
+        dialMode = 1;   
+        Serial.println("Pan Dial Mode: On");
+    }
+    else if  ( request.indexOf("TiltDialMode") > 0 )  { 
+        dialMode = 2;  
+        Serial.println("Tilt Dial Mode: On"); 
+    }
+    else if  ( request.indexOf("DisableAll") > 0 )  { 
+        dialMode = 0; 
+        Serial.println("All dial modes are disabled");
+    }
+    else if  ( request.indexOf("EnableAll") > 0 )  { 
+        dialMode = 3 ;   
+        Serial.println("All dial modes are enabled");
+    }
+    
+    client.flush();
+    
+    
+    for (int i = 0; i < sizeOfWebPage; i++){
+      client.print(webPage[i]);
+    }
+    
+    delay(5);
+  // The client will actually be disconnected when the function returns and 'client' object is detroyed
   
-  /*panMotor.write(translateVoltage(analogRead(A0)));
-  delay(100);
- */
- /*RGB_color(255, 255, 125); // Raspberry
- delay(1000);
- RGB_color(0, 255, 255); // Cyan
- delay(1000);
- RGB_color(255, 255, 0); // Yellow
- delay(1000);
- RGB_color(80,0,80); //purple
- delay(1000);
- */
- /*panMotor.write(90);
- delay(1000);
- panMotor.write(0);
- delay(1000);
- tiltMotor.write(90);
- delay(1000);
- tiltMotor.write(0);
- delay(1000);
- */
-} 
+}
